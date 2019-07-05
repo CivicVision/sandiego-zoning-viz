b51bf3d8b6daab37daa6fb6dad7fd42ea7566f07
Mila Frerichs <mila.frerichs@gmail.com>
2019-07-05 12:23:42 +0200

    Add styling and make map full width
    leave the code for dowmnloding in, but htis will be the interactive
    version

--- index.js
+++ index.js
@@ -1,38 +1,38 @@
-require("https://d3js.org/d3.v5.min.js")
-require("topojson-client@3")
+async function createChart() {
+  var sandiego_buildings = await d3.json('https://open-data-cv.s3.amazonaws.com/sandiego_buildings.json')
+  var sandiego_features = topojson.feature(sandiego_buildings, sandiego_buildings.objects.buildings).features.filter(d => "zone_name" in d.properties)
+  var buildings = { type: "FeatureCollection", features: sandiego_features }
 
-var sandiego_buildings = d3.json('https://open-data-cv.s3.amazonaws.com/sandiego_buildings.json')
-var sandiego_features = topojson.feature(sandiego_buildings, sandiego_buildings.objects.buildings).features.filter(d => "zone_name" in d.properties)
-var buildings = { type: "FeatureCollection", features: sandiego_features }
-
-const height = Math.round(900 / 960 * width);
+  var bbox = d3.select('#map').node().getBoundingClientRect()
+  const width = bbox.width;
+  const height = bbox.height;
   const indicator = 'zone_name';
   const noDataColor = '#ccc';
   const projection = d3.geoAlbers();
-  const scale = width / 960;
   projection.fitExtent([[20, 20], [width, height]], buildings);
-  
-  function colorForEntry(d) { 
-    if(indicator in d.properties) { 
+
+  function colorForEntry(d) {
+    if(indicator in d.properties) {
       if(d.properties[indicator].match(/RS/))
       {
-        return '#EA60B9'; 
+        return '#EA60B9';
       }
       if(d.properties[indicator].match(/RE|RX|RT|RM/))
       {
-        return '#4CAFC5'; 
-       }
+        return '#4CAFC5';
+      }
     }
     return noDataColor;
   }
-  
+
+  d3.select("#map").html('');
+  const canvas = d3.select("#map").append("canvas").attr("width", width).attr("height", height)
   const context = canvas.node().getContext("2d")
-  canvas = d3.select("body").append("canvas").attr("width", width).attr("height", height)
-  
+
   function draw() {
     const path = d3.geoPath()
-    .projection(projection)
-    .context(context)
+      .projection(projection)
+      .context(context)
     buildings.features.forEach(function(d) {
       context.beginPath();
       context.fillStyle = colorForEntry(d);
@@ -43,7 +43,7 @@ const height = Math.round(900 / 960 * width);
       context.stroke();
     });
   }
-  
+
   function zoom() {
     var transform = d3.event.transform;
     context.save();
@@ -53,15 +53,26 @@ const height = Math.round(900 / 960 * width);
     draw();
     context.restore();
   }
-  
-  
-  
+
   canvas.call(d3.zoom().scaleExtent([1, 8]).on("zoom", zoom))
-  
-  context.scale(scale, scale);
+
   context.lineJoin = "round";
   context.lineCap = "round";
-  
+
   draw()
-  
+  return canvas;
+}
+function downloadImage(canvas) {
+  var link = document.createElement("a");
+  link.download = "sandiego.png";
+
+  canvas.toBlob(function(blob){
+    link.href = URL.createObjectURL(blob);
+    link.click();
+  },'image/png');
+}
+const canvas = createChart();
+async function download() {
+  const node = await canvas;
+  downloadImage(node.node());
 }
